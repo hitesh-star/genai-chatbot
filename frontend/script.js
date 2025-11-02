4daef0af040792406d88d4760019f663404731f8
Hitesh <hitesh.parashar2016@gmail.com>
2025-11-02 13:39:14 +0530

    Chatbot UI

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,37 @@
-console.log("working")
+const input=document.querySelector("#input");
+const chatContainer=document.querySelector("#chat-container");
+const askBtn=document.querySelector('#ask');
+
+input?.addEventListener('keyup',handleEnter);
+askBtn.addEventListener('click',handleAsk);
+
+function generate(text){
+  /**
+   * 1.append message to UI
+   * 2.Send it to LLM
+   * 3.Append message to UI
+   */
+  const msg=document.createElement('div');
+  msg.className="my-6 bg-neutral-800 p-3 rounded-xl ml-auto max-w-fit";
+  msg.textContent=text;
+  chatContainer?.appendChild(msg);
+  input.value='';
+}
+
+function handleAsk(){
+     const text=input?.value.trim();
+     if(!text){
+        return;
+     }
+     generate(text);
+}
+
+function handleEnter(e){
+   if(e.key==="Enter"){
+     const text=input?.value.trim();
+     if(!text){
+        return;
+     }
+     generate(text);
+   }
+}
